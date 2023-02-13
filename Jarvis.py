da42acda05cd02e3c44e71647e7db9158cc7f98f
RaffLB <raffertyberesford08@gmail.com>
2023-02-13 17:59:40 +0000

    Using Azure directly

--- Jarvis.py
+++ Jarvis.py
@@ -1,3 +1,4 @@
+import azure.cognitiveservices.speech as speechsdk
 from phue import Bridge
 import speech_recognition as sr
 import pyttsx3
@@ -43,30 +44,53 @@ def talk(text):
     engine.runAndWait()
 
 def take_command():
-    command_was_for_jarvis = False
-    try:
-        with sr.Microphone() as source:
-            the_boss = os.environ.get("the_boss")
-            talk ("How may I assist you " + the_boss + "?")
-            while command_was_for_jarvis == False:
-                print ("Listening...")            
-                voice = listener.listen(source)
-                command = listener.recognize_google(voice)
-                command = command.lower()
-                if "jarvis" in command:
-                    command_was_for_jarvis = True
-                    command = command.replace("jarvis", "")
-                    print(command)
-                else:
-                    print("This command wasn't for Jarvis")
+    the_boss = os.environ.get("the_boss")
+    talk ("How may I assist you " + the_boss + "?")
+    command = recognize_from_microphone()
+    print("take command heard: " + command)
+    return command.lower()
 
-    except AssertionError as error:
-        print("Exception in 'take_command' " + error)
-        pass
-    return command
+def wait_for_instruction():
+    command_was_for_jarvis = False
+    while command_was_for_jarvis == False:
+        print ("Listening for wake word...")
+        command = recognize_from_microphone()
+        print("wait_for_instruction heard: " + command)
+        command = command.lower()
+        if "jarvis" in command:
+            command_was_for_jarvis = True
+        else:
+            print(".", end = '')
+
+    return command_was_for_jarvis
+
+def recognize_from_microphone():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    azure_cognitiveservices_key = os.environ.get("azure_cognitiveservices_key")
+    speech_config = speechsdk.SpeechConfig(subscription=azure_cognitiveservices_key, region="uksouth")
+    speech_config.speech_recognition_language="en-GB"
+
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    print("Speak into your microphone.")
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(speech_recognition_result.text))
+        return (speech_recognition_result.text)
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
 
 def run_jarvis():
     user_wants_to_continue = True
+    wait_for_instruction()
     command = take_command()
     print(command)
     if "play" in command:
@@ -133,6 +157,8 @@ def search_google (search_item):
 
 # Main loop
 openai_key = os.environ.get("openai_key")
+
+
 
 print("")
 should_i_continue = True
